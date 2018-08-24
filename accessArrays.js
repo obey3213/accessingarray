997a43776f472bb07ba0d57ef9bf9fd781f9825c
Michael Harmon <38897469+obey3213@users.noreply.github.com>
2018-08-23 18:30:36 -0600

    better

--- accessArrays.js
+++ accessArrays.js
@@ -22,13 +22,14 @@ console.log(arr[i]);
 for(let i = 0; i < arr.length; i++){
     console.log(arr[i])
 };
-//console log accountNumber
-const response = data[0].accountNumber
+// console log accountNumber
+const answer1 = data[0].accountNumber
+console.log(answer1);
 
-console.log(response);
-
+// console log the accounts array length
 const answer2 = data[0].accounts.length;
-console.log(answer2);
+console.log(answer2)
 
-const answer3 = data[0].accounts[1].name;
-console.log(answer3);
+// console log the second acount's name
+const answer3 = data[0].accounts[1].name
+console.log(answer3)
